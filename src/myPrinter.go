596f592e0763a63c50b92771bc3e2bb7b028008f
Liu-YT <1039868176@qq.com>
2018-10-24 17:11:49 +0800

    little change

--- src/myPrinter.go
+++ src/myPrinter.go
@@ -33,28 +33,3 @@ func main() {
 		writer.Flush()
 	}
 }
-
-// package main
-
-// func main() {
-
-// 	reader := bufio.NewReader(os.Stdin)
-// 	f, err := os.OpenFile("receiverText.txt", os.O_WRONLY, 0666)
-
-// 	if err != nil {
-// 		os.Stderr.WriteString("fail to open receiver")
-// 	}
-// 	writer := bufio.NewWriter(f)
-// 	for {
-// 		line, err := reader.ReadBytes('\n')
-// 		if err != nil { //遇到任何错误立即返回，并忽略 EOF 错误信息
-// 			if err == io.EOF {
-// 				break
-// 			}
-// 			os.Stderr.Write([]byte("read bytes from Reader error\n"))
-// 			os.Exit(5)
-// 		}
-// 		writer.Write(line)
-// 		writer.Flush()
-// 	}
-// }
